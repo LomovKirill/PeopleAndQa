61d13971805f771719607ddddfb70fa8412b52f5
LomovKirill <Heskey41@gmail.com>
2021-08-29 16:02:16 +0300

    add Task 16

---
+++ People.java
@@ -0,0 +1,11 @@
+public class People {
+    public void sleep(){
+        System.out.println("Я сплю");
+    }
+    public void drink(){
+        System.out.println("Я пью");
+    }
+    public void run(){
+        System.out.println("Я бегу");
+    }
+}
---
+++ Qa.java
@@ -0,0 +1,9 @@
+public class Qa extends People {
+    public void testing(){
+        System.out.println("Тестировщик тестирует");
+    }
+    public void flooding(){
+        System.out.println("Тестировщик общается");
+    }
+
+}
